3d45fff341292a7e946b7f9a909dbf8fc5f47656
EricMendoza412 <gpericmendoza@gmail.com>
2022-09-23 15:10:13 +0800

    For dummy account bug fix

--- pawnshop_management/pawnshop_management/doctype/provisional_receipt/provisional_receipt.js
+++ pawnshop_management/pawnshop_management/doctype/provisional_receipt/provisional_receipt.js
@@ -592,11 +592,15 @@ function show_fields_for_dummy(frm) {
 	if (frm.doc.pawn_ticket_type == 'Pawn Ticket Jewelry') {
 		cur_frm.clear_table('actual_items_nj')
 		frm.refresh_field('actual_items_nj')
-		frm.toggle_reqd('actual_items_j', true)
+		if(frm.doc.transaction_type == 'Renewal' || frm.doc.transaction_type == 'Renewal w/ Amortization') {
+			frm.toggle_reqd('actual_items_j', true)
+		}
 	} else if (frm.doc.pawn_ticket_type == 'Pawn Ticket Non Jewelry') {
 		cur_frm.clear_table('actual_items_j')
 		frm.refresh_field('actual_items_j')
+		if(frm.doc.transaction_type == 'Renewal' || frm.doc.transaction_type == 'Renewal w/ Amortization') {
 		frm.toggle_reqd('actual_items_nj', true)
+		}
 	}
 }
 
