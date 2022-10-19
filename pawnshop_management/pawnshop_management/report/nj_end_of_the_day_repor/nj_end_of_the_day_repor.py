7a232914c2281ccda244fe5736bfaa4b4b63fd19
EricMendoza412 <gpericmendoza@gmail.com>
2022-10-19 16:50:08 +0800

    for NJ end of the day report filter

--- pawnshop_management/pawnshop_management/report/nj_end_of_the_day_repor/nj_end_of_the_day_repor.py
+++ pawnshop_management/pawnshop_management/report/nj_end_of_the_day_repor/nj_end_of_the_day_repor.py
@@ -8,15 +8,15 @@ from pawnshop_management.pawnshop_management.custom_codes.get_ip import get_ip_f
 def execute(filters=None):
 	current_ip = frappe.local.request_ip
 	branch_ip = get_ip_from_settings()
-	if str(current_ip) == str(branch_ip['cavite_city']) and (user_role.role_profile_name == "Cashier" or user_role.role_profile_name == "Supervisor/Cashier" or user_role.role_profile_name == "Appraiser/Cashier" or user_role.role_profile_name == "Appraiser" or user_role.role_profile_name == "Supervisor"):
+	if str(current_ip) == str(branch_ip['cavite_city']):
 		branch = "Garcia''s Pawnshop - CC"
-	elif str(current_ip) == str(branch_ip['poblacion']) and (user_role.role_profile_name == "Cashier" or user_role.role_profile_name == "Supervisor/Cashier" or user_role.role_profile_name == "Appraiser/Cashier" or user_role.role_profile_name == "Appraiser" or user_role.role_profile_name == "Supervisor"):
+	elif str(current_ip) == str(branch_ip['poblacion']):
 		branch = "Garcia''s Pawnshop - POB"
-	elif str(current_ip) == str(branch_ip['molino']) and (user_role.role_profile_name == "Cashier" or user_role.role_profile_name == "Supervisor/Cashier" or user_role.role_profile_name == "Appraiser/Cashier" or user_role.role_profile_name == "Appraiser" or user_role.role_profile_name == "Supervisor"):
+	elif str(current_ip) == str(branch_ip['molino']):
 		branch = "Garcia''s Pawnshop - MOL"
-	elif str(current_ip) == str(branch_ip['gtc']) and (user_role.role_profile_name == "Cashier" or user_role.role_profile_name == "Supervisor/Cashier" or user_role.role_profile_name == "Appraiser/Cashier" or user_role.role_profile_name == "Appraiser" or user_role.role_profile_name == "Supervisor"):
+	elif str(current_ip) == str(branch_ip['gtc']):
 		branch = "Garcia''s Pawnshop - GTC"
-	elif str(current_ip) == str(branch_ip['tanza']) and (user_role.role_profile_name == "Cashier" or user_role.role_profile_name == "Supervisor/Cashier" or user_role.role_profile_name == "Appraiser/Cashier" or user_role.role_profile_name == "Appraiser" or user_role.role_profile_name == "Supervisor"):
+	elif str(current_ip) == str(branch_ip['tanza']):
 		branch = "Garcia''s Pawnshop - TNZ"
 	
 	columns, data = [], []
