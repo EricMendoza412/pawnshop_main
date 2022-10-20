151ef807aee50a9fb9bbf5ad2669878b9addd252
EricMendoza412 <gpericmendoza@gmail.com>
2022-10-20 10:40:56 +0800

    NJ end of day filter

--- pawnshop_management/pawnshop_management/report/nj_end_of_the_day_repor/nj_end_of_the_day_repor.js
+++ pawnshop_management/pawnshop_management/report/nj_end_of_the_day_repor/nj_end_of_the_day_repor.js
@@ -2,26 +2,26 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["NJ End of the Day Repor"] = {
-	"filters": [
-		{
-			fieldname: "branch",
-			label: __("Branch"),
-			fieldtype: "Select",
-			options: [
-				"Garcia's Pawnshop - CC", 
-				"Garcia's Pawnshop - GTC", 
-				"Garcia's Pawnshop - MOL",
-				"Garcia's Pawnshop - POB",
-				"Garcia's Pawnshop - TNZ",
-				"Rabie's House"
-			]
-		},
+// frappe.query_reports["NJ End of the Day Repor"] = {
+// 	"filters": [
+// 		{
+// 			fieldname: "branch",
+// 			label: __("Branch"),
+// 			fieldtype: "Select",
+// 			options: [
+// 				"Garcia's Pawnshop - CC", 
+// 				"Garcia's Pawnshop - GTC", 
+// 				"Garcia's Pawnshop - MOL",
+// 				"Garcia's Pawnshop - POB",
+// 				"Garcia's Pawnshop - TNZ",
+// 				"Rabie's House"
+// 			]
+// 		},
 
-		{
-			fieldname: "date_loan_granted",
-			label: __("Date Loan Granted"),
-			fieldtype: "Date"
-		}
-	]
-};
+// 		{
+// 			fieldname: "date_loan_granted",
+// 			label: __("Date Loan Granted"),
+// 			fieldtype: "Date"
+// 		}
+// 	]
+// };
