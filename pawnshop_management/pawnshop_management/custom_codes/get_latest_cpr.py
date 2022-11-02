ed262796fa7f88f5853f962a2a68cc457a4f26c5
EricMendoza412 <gpericmendoza@gmail.com>
2022-11-02 17:18:12 +0800

    CPR fix total_cash = beg bal

--- pawnshop_management/pawnshop_management/custom_codes/get_latest_cpr.py
+++ pawnshop_management/pawnshop_management/custom_codes/get_latest_cpr.py
@@ -4,5 +4,5 @@ import frappe
 def get_latest_cpr(branch):
     if frappe.db.exists('Cash Position Report', {'branch': branch}):
         cpr = frappe.get_last_doc('Cash Position Report', filters={"branch": branch})
-        return cpr.ending_balance
+        return cpr.total_cash
         
