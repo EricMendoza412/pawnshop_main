1eb490e22632871a232328b2c5a24c89ef3577c5
EricMendoza412 <gpericmendoza@gmail.com>
2023-01-19 15:27:47 +0800

    For Jewelry end of day report

--- pawnshop_management/pawnshop_management/report/j_end_of_day_report/j_end_of_day_report.js
+++ pawnshop_management/pawnshop_management/report/j_end_of_day_report/j_end_of_day_report.js
@@ -1,9 +1,69 @@
 // Copyright (c) 2016, Rabie Moses Santillan and contributors
 // For license information, please see license.txt
 /* eslint-disable */
+var default_branch = [];
 
+frappe.call({
+    method: 'pawnshop_management.pawnshop_management.custom_codes.get_ip.get_ip',
+    callback: function(data){
+        let current_ip = data.message
+        frappe.call({
+            method: 'pawnshop_management.pawnshop_management.custom_codes.get_ip.get_ip_from_settings',
+            callback: (result) => {
+                let ip = result.message;
+                if (current_ip == ip["cavite_city"]) {
+                    default_branch.push("Garcia's Pawnshop - CC"); 
+                } else if (current_ip == ip["poblacion"]) {
+                    default_branch.push("Garcia's Pawnshop - POB");
+                } else if (current_ip == ip["molino"]) {
+                    default_branch.push("Garcia's Pawnshop - MOL");
+                } else if (current_ip == ip["gtc"]) {
+                    default_branch.push("Garcia's Pawnshop - GTC");
+                } else if (current_ip == ip["tanza"]) {
+                    default_branch.push("Garcia's Pawnshop - TNZ");
+                }
+            }
+        })
+    }
+});
+
+let is_allowed = frappe.user_roles.includes('Auditor') || frappe.user_roles.includes('Administrator');
+if(is_allowed){
 frappe.query_reports["J End of Day Report"] = {
 	"filters": [
+		{
+			fieldname: "branch",
+			label: __("Branch"),
+			fieldtype: "Select",
+			options: [
+				"Garcia's Pawnshop - CC", 
+				"Garcia's Pawnshop - GTC", 
+				"Garcia's Pawnshop - MOL",
+				"Garcia's Pawnshop - POB",
+				"Garcia's Pawnshop - TNZ",
+			],
+			default: default_branch
+		}
 
+		 ,{
+		 	fieldname: "item_series",
+		 	label: __("Status"),
+		 	fieldtype: "Select",
+			 options: ["A", "B"],
+			default: "A"
+		 }
 	]
-};
+}
+}else{
+frappe.query_reports["J End of Day Report"] = {
+	"filters": [
+		{
+			fieldname: "branch",
+			label: __("Branch"),
+			fieldtype: "Data",
+			default: default_branch
+		}
+	]
+}
+
+}
