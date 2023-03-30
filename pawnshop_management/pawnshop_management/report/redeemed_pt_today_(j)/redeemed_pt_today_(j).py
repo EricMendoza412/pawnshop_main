f94736c65b654af28ef4ca27317f6f98c8495e85
EricMendoza412 <gpericmendoza@gmail.com>
2023-03-30 17:44:48 +0800

    For redeemed PT report

---
+++ pawnshop_management/pawnshop_management/report/redeemed_pt_today_(j)/redeemed_pt_today_(j).py
@@ -0,0 +1,70 @@
+# Copyright (c) 2023, Rabie Moses Santillan and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from pawnshop_management.pawnshop_management.custom_codes.get_ip import get_ip_from_settings
+
+
+def execute(filters=None):
+	columns, data = [], []
+	branch = ""
+	current_ip = frappe.local.request_ip
+	branch_ip = get_ip_from_settings()
+	if str(current_ip) == str(branch_ip['cavite_city']):
+		branch = "Garcia's Pawnshop - CC"
+	elif str(current_ip) == str(branch_ip['poblacion']):
+		branch = "Garcia's Pawnshop - POB"
+	elif str(current_ip) == str(branch_ip['molino']):
+		branch = "Garcia's Pawnshop - MOL"
+	elif str(current_ip) == str(branch_ip['gtc']):
+		branch = "Garcia's Pawnshop - GTC"
+	elif str(current_ip) == str(branch_ip['tanza']):
+		branch = "Garcia's Pawnshop - TNZ"
+
+	
+	data = frappe.db.sql("""
+		SELECT workflow_state, pawn_ticket, date_loan_granted, desired_principal
+		FROM `tabPawn Ticket Jewelry`
+		WHERE `tabPawn Ticket Jewelry`.docstatus=1
+		AND `tabPawn Ticket Jewelry`.workflow_state = 'Redeemed'
+		AND `tabPawn Ticket Jewelry`.change_status_date = CURDATE()
+		AND `tabPawn Ticket Jewelry`.branch = %s
+	""", (branch), as_dict=True)
+
+	columns = get_columns()
+	return columns, data
+
+def get_columns():
+	columns = [
+
+		{
+			'fieldname': 'workflow_state',
+			'label': _('Status'),
+			'fieldtype': 'Data',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'pawn_ticket',
+			'label': _('Pawn Ticket'),
+			'fieldtype': 'Data',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'date_loan_granted',
+			'label': _('Date Loan Granted'),
+			'fieldtype': 'Data',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'desired_principal',
+			'label': _('Principal'),
+			'fieldtype': 'Data',
+			'width': 200
+		}
+		
+	]
+	return columns
