9a22d7a71f58612a52359dc5d2de399eac51c2ae
EricMendoza412 <gpericmendoza@gmail.com>
2023-03-30 17:52:40 +0800

    Vc Report combination

---
+++ pawnshop_management/pawnshop_management/report/vc_report/vc_report.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2023, Rabie Moses Santillan and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from pawnshop_management.pawnshop_management.custom_codes.get_ip import get_ip_from_settings
+
+
+def execute(filters=None):
+	columns, data = [], []
+	branch = ""
+	current_ip = frappe.local.request_ip
+	branch_ip = get_ip_from_settings()
+	if str(current_ip) == str(branch_ip['cavite_city']):
+		branch = "Garcia's Pawnshop - CC"
+	elif str(current_ip) == str(branch_ip['poblacion']):
+		branch = "Garcia's Pawnshop - POB"
+	elif str(current_ip) == str(branch_ip['molino']):
+		branch = "Garcia's Pawnshop - MOL"
+	elif str(current_ip) == str(branch_ip['gtc']):
+		branch = "Garcia's Pawnshop - GTC"
+	elif str(current_ip) == str(branch_ip['tanza']):
+		branch = "Garcia's Pawnshop - TNZ"
+		
+	data_redeemed = frappe.db.sql("""
+		SELECT '' as old_pawn_ticket, workflow_state, pawn_ticket, date_loan_granted, desired_principal
+		FROM `tabPawn Ticket Jewelry`
+		WHERE `tabPawn Ticket Jewelry`.docstatus=1
+		AND `tabPawn Ticket Jewelry`.workflow_state = 'Redeemed'
+		AND `tabPawn Ticket Jewelry`.change_status_date = CURDATE()
+		AND `tabPawn Ticket Jewelry`.branch = %s
+	""", (branch), as_dict=True)
+	
+	data_renewed = frappe.db.sql("""
+		SELECT old_pawn_ticket, workflow_state, pawn_ticket, date_loan_granted, desired_principal
+		FROM `tabPawn Ticket Jewelry`
+		WHERE `tabPawn Ticket Jewelry`.docstatus=1
+		AND `tabPawn Ticket Jewelry`.old_pawn_ticket IS NOT NULL
+		AND `tabPawn Ticket Jewelry`.date_loan_granted = CURDATE()
+		AND `tabPawn Ticket Jewelry`.branch = %s
+	""", (branch), as_dict=True)
+
+	data = data_redeemed + data_renewed
+
+	columns = get_columns()
+	return columns, data
+
+def get_columns():
+	columns = [
+		{
+			'fieldname': 'old_pawn_ticket',
+			'label': _('Old Pawn Ticket'),
+			'fieldtype': 'Data',
+			'width': 100
+		},
+
+		{
+			'fieldname': 'workflow_state',
+			'label': _('Status'),
+			'fieldtype': 'Data',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'pawn_ticket',
+			'label': _('Pawn Ticket'),
+			'fieldtype': 'Data',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'date_loan_granted',
+			'label': _('Date Loan Granted'),
+			'fieldtype': 'Date',
+			'width': 200
+		},
+
+		{
+			'fieldname': 'desired_principal',
+			'label': _('New PT Principal'),
+			'fieldtype': 'Data',
+			'width': 200
+		}
+		
+	]
+	return columns
