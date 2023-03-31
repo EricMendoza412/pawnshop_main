50f017ab15b0be34d953659aa22a201f2dbd5f85
EricMendoza412 <gpericmendoza@gmail.com>
2023-03-31 15:13:28 +0800

    New sangla report added

---
+++ pawnshop_management/pawnshop_management/report/new_sangla_today_(j)/new_sangla_today_(j).js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Rabie Moses Santillan and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["New Sangla today (J)"] = {
+	"filters": [
+
+	]
+};
