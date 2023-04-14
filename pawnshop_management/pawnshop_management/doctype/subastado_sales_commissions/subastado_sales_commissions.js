4295d3881ff40f367bc9cf0c0c3ce2aa68a4d438
EricMendoza412 <gpericmendoza@gmail.com>
2023-04-14 11:49:08 +0800

    For subastado sales

---
+++ pawnshop_management/pawnshop_management/doctype/subastado_sales_commissions/subastado_sales_commissions.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Rabie Moses Santillan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Subastado Sales Commissions', {
+	// refresh: function(frm) {
+
+	// }
+});
