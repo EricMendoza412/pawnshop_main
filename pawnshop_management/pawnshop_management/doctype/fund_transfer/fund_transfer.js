4bbd549393020ff7e763b79c4db0557a447c789e
EricMendoza412 <gpericmendoza@gmail.com>
2023-06-28 23:59:28 +0800

    for Fund transfer doctype

---
+++ pawnshop_management/pawnshop_management/doctype/fund_transfer/fund_transfer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Rabie Moses Santillan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fund Transfer', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pawnshop_management/pawnshop_management/doctype/fund_transfer/test_fund_transfer.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Rabie Moses Santillan and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestFundTransfer(unittest.TestCase):
+	pass
