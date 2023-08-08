e7150afe6a3b865539b2654803802f568f52657c
EricMendoza412 <gpericmendoza@gmail.com>
2023-08-08 11:33:16 +0800

    Models auto Min and Def computation

--- pawnshop_management/pawnshop_management/doctype/models/models.js
+++ pawnshop_management/pawnshop_management/doctype/models/models.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Models', {
+
+
+	maximum: function(frm){
+		frm.set_value('minimum', frm.doc.maximum * 0.8);
+		frm.set_value('defective', frm.doc.maximum * 0.5);
+	}
+
 	// refresh: function(frm) {
 
 	// }
+
+
 });
