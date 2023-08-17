5f1724eeb0c1c344d46df058d8acf6137e9c9242
EricMendoza412 <gpericmendoza@gmail.com>
2023-08-17 10:26:12 +0800

    fix Def and Min auto

--- pawnshop_management/pawnshop_management/doctype/models/models.js
+++ pawnshop_management/pawnshop_management/doctype/models/models.js
@@ -5,8 +5,8 @@ frappe.ui.form.on('Models', {
 
 
 	maximum: function(frm){
-		frm.set_value('minimum', frm.doc.maximum * 0.8);
-		frm.set_value('defective', frm.doc.maximum * 0.5);
+		frm.set_value('minimum', Math.ceil(frm.doc.maximum * 0.8 / 100) * 100);
+		frm.set_value('defective', Math.ceil(frm.doc.maximum * 0.5 / 100) * 100);
 	}
 
 	// refresh: function(frm) {
