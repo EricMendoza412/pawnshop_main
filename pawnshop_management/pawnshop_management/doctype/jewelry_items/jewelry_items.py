d7550f2d7a63dc33fd95daaa1e019361d9dec079
Ericson Mendoza <gpericmendoza@gmail.com>
2024-03-15 15:12:07 +0800

    For edit access debugging

--- pawnshop_management/pawnshop_management/doctype/jewelry_items/jewelry_items.py
+++ pawnshop_management/pawnshop_management/doctype/jewelry_items/jewelry_items.py
@@ -103,56 +103,56 @@ class JewelryItems(Document):
 				parent_doc = frappe.get_doc("Agreement to Sell", acc_parent_doc_name)
 				parent_ats_name = parent_doc.name
 
-			frappe.db.set_value('Agreement to Sell', parent_ats_name, 'workflow_state', 'Rejected')
-			frappe.db.set_value('Agreement to Sell', parent_ats_name, 'change_status_date', today())
-			frappe.db.set_value('Agreement to Sell', parent_ats_name, 'docstatus', 2)
-			frappe.db.commit()
+				frappe.db.set_value('Agreement to Sell', parent_ats_name, 'workflow_state', 'Rejected')
+				frappe.db.set_value('Agreement to Sell', parent_ats_name, 'change_status_date', today())
+				frappe.db.set_value('Agreement to Sell', parent_ats_name, 'docstatus', 2)
+				frappe.db.commit()
 
-			# transfer PT details to the next version 
-			previous_ats = frappe.get_doc("Agreement to Sell", parent_ats_name)
-			new_ats = frappe.new_doc("Agreement to Sell")
-			new_ats.branch = previous_ats.branch
-			if previous_ats.amended_from == None:
-				new_ats.form_number= str(previous_ats.name) + '-1'
-			else: 
-				split_old_pt = previous_ats.name.split('-')
-				split_old_pt[2] = int(split_old_pt[2]) + 1
-				new_ats.form_number = split_old_pt[0] + '-' + split_old_pt[1] + '-' + str(split_old_pt[2]) 
-			new_ats.amended_from = previous_ats.name
-			new_ats.date_of_sale = previous_ats.date_of_sale
-			new_ats.customer_tracker = previous_ats.customer_tracker
-			new_ats.customer_name = previous_ats.customer_name
-			new_ats.ats_tracking_no = previous_ats.ats_tracking_no
-			new_ats.agreement = previous_ats.agreement
-			previous_items = previous_ats.jewelry_items
-			
-			for i in range(len(previous_items)):
-				new_ats.append("jewelry_items", {
-					"item_no": previous_items[i].item_no,
-					"type": previous_items[i].type,
-					"karat_category": previous_items[i].karat_category,
-					"karat": previous_items[i].karat,
-					"weight": previous_items[i].weight,
-					"color": previous_items[i].color,
-					"colors_if_multi": previous_items[i].colors_if_multi,
-					"additional_for_stone": previous_items[i].additional_for_stone,
-					"suggested_appraisal_value": previous_items[i].suggested_appraisal_value,
-					"desired_principal": previous_items[i].desired_principal,
-					"comments": previous_items[i].comments
-				})
-			new_ats.total_value = previous_ats.total_value
-			new_ats.seller_name_and_signature = previous_ats.seller_name_and_signature
-			new_ats.cashier_name_and_signature = previous_ats.cashier_name_and_signature
-			new_ats.appraiser_name_and_signature = previous_ats.appraiser_name_and_signature
-			new_ats.save(ignore_permissions=True)
+				# transfer PT details to the next version 
+				previous_ats = frappe.get_doc("Agreement to Sell", parent_ats_name)
+				new_ats = frappe.new_doc("Agreement to Sell")
+				new_ats.branch = previous_ats.branch
+				if previous_ats.amended_from == None:
+					new_ats.form_number= str(previous_ats.name) + '-1'
+				else: 
+					split_old_pt = previous_ats.name.split('-')
+					split_old_pt[2] = int(split_old_pt[2]) + 1
+					new_ats.form_number = split_old_pt[0] + '-' + split_old_pt[1] + '-' + str(split_old_pt[2]) 
+				new_ats.amended_from = previous_ats.name
+				new_ats.date_of_sale = previous_ats.date_of_sale
+				new_ats.customer_tracker = previous_ats.customer_tracker
+				new_ats.customer_name = previous_ats.customer_name
+				new_ats.ats_tracking_no = previous_ats.ats_tracking_no
+				new_ats.agreement = previous_ats.agreement
+				previous_items = previous_ats.jewelry_items
+				
+				for i in range(len(previous_items)):
+					new_ats.append("jewelry_items", {
+						"item_no": previous_items[i].item_no,
+						"type": previous_items[i].type,
+						"karat_category": previous_items[i].karat_category,
+						"karat": previous_items[i].karat,
+						"weight": previous_items[i].weight,
+						"color": previous_items[i].color,
+						"colors_if_multi": previous_items[i].colors_if_multi,
+						"additional_for_stone": previous_items[i].additional_for_stone,
+						"suggested_appraisal_value": previous_items[i].suggested_appraisal_value,
+						"desired_principal": previous_items[i].desired_principal,
+						"comments": previous_items[i].comments
+					})
+				new_ats.total_value = previous_ats.total_value
+				new_ats.seller_name_and_signature = previous_ats.seller_name_and_signature
+				new_ats.cashier_name_and_signature = previous_ats.cashier_name_and_signature
+				new_ats.appraiser_name_and_signature = previous_ats.appraiser_name_and_signature
+				new_ats.save(ignore_permissions=True)
 
-			#recompute for Total after saving and before submitting
-			new_items = new_ats.jewelry_items
-			total_amount = 0
-			for items in new_items:
-				total_amount = total_amount + items.desired_principal
-			new_ats.total_value = total_amount
-			new_ats.submit()
+				#recompute for Total after saving and before submitting
+				new_items = new_ats.jewelry_items
+				total_amount = 0
+				for items in new_items:
+					total_amount = total_amount + items.desired_principal
+				new_ats.total_value = total_amount
+				new_ats.submit()
 
 
 
