04956cc09bae574c43bb4054d2be51f4b40bbc7f
Ericson Mendoza <gpericmendoza@gmail.com>
2024-08-20 19:36:33 +0800

    Transfer tracker doctype upload

---
+++ pawnshop_management/pawnshop_management/doctype/transfer_tracker/transfer_tracker.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Rabie Moses Santillan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Transfer Tracker', {
+	// refresh: function(frm) {
+
+	// }
+});
