d6034022a3355d3581d66dc185b56543caf11956
Ericson Mendoza <gpericmendoza@gmail.com>
2025-05-12 08:36:37 +0800

    IT memo 5.12.25

--- pawnshop_management/pawnshop_management/doctype/models/models.js
+++ pawnshop_management/pawnshop_management/doctype/models/models.js
@@ -1,25 +1,39 @@
 // Copyright (c) 2022, Rabie Moses Santillan and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on('Models', {
 
+    maximum: function(frm) {
 
-	maximum: function(frm){
-		frm.set_value('minimum', Math.ceil(frm.doc.maximum * 0.8 / 100) * 100);
-		frm.set_value('defective', Math.ceil(frm.doc.maximum * 0.5 / 100) * 100);
-	},
+        if (frm.doc.maximum > 0) {
+            frm.set_value('minimum', Math.ceil(frm.doc.maximum * 0.8 / 100) * 100);
+            frm.set_value('defective', Math.ceil(frm.doc.maximum * 0.5 / 100) * 100);
+        }
+    },
 
-	max_sell_price: function(frm){
-		if(frm.doc.max_av_percent != 0){
-			frm.set_value('maximum', Math.ceil(frm.doc.max_sell_price * (frm.doc.max_av_percent * 0.01) / 100) * 100);
-		}else{
-			frm.set_value('maximum', Math.ceil(frm.doc.max_sell_price * 0.8 / 100) * 100);
+    max_sell_price: function(frm) {
+
+		frm.set_value('last_srp_update', frappe.datetime.get_today());
+
+        if (frm.doc.max_sell_price > 0 && frm.doc.max_av_percent > 0) {
+            frm.set_value('maximum', Math.ceil(frm.doc.max_sell_price * (frm.doc.max_av_percent * 0.01) / 100) * 100);
+        }else{
+			frm.set_value('maximum', Math.ceil(frm.doc.max_sell_price * (80 * 0.01) / 100) * 100);
+		}
+    },
+
+	max_av_percent: function(frm) {
+
+        if (frm.doc.max_sell_price > 0 && frm.doc.max_av_percent > 0) {
+            frm.set_value('maximum', Math.ceil(frm.doc.max_sell_price * (frm.doc.max_av_percent * 0.01) / 100) * 100);
+        }else{
+			frm.set_value('maximum', Math.ceil(frm.doc.max_sell_price * (80 * 0.01) / 100) * 100);
 		}
 	}
 
-	// refresh: function(frm) {
+    // refresh: function(frm) {
 
-	// }
-
+    // }
 
 });
